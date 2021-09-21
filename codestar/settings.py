cb139052c0c45f972a4838ac83fab01c4cc0035d
KSanthanesh <santhanesh_2004@yahoo.com>
2021-09-21 19:13:22 +0000

    Add deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar-2021@herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestar-2021.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -137,7 +137,7 @@ STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')]
 
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
